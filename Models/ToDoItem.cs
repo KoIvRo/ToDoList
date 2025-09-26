c7dec2829ec867fe52be0ae0ae8dbce63fb1153d
KoIvRo <korot4ev29@gmail.com>
2025-09-26 17:49:50 +1000

    Добавление. Удаление. Фильтр

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToDoList.Models
+{
+    public partial class ToDoItem : ObservableObject
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [ObservableProperty]
+        private string _itemName;
+
+
+        [ObservableProperty]
+        private string _category;
+
+
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,95 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly ToDoDB _database;
+
+        public List<string> Categories { get; } = new()
+        {
+            "Дом",
+            "Учеба",
+            "Работа",
+            "Личное",
+            "Другое"
+        };
+
+        public List<string> FilterCategories { get; } = new()
+        {
+            "Все",
+            "Дом",
+            "Учеба",
+            "Работа",
+            "Личное",
+            "Другое"
+        };
+
+
+        [ObservableProperty]
+        private string selectedCategory = "Все";
+
+        [ObservableProperty]
+        private ObservableCollection<ToDoItem> todoItems;
+
+        [ObservableProperty]
+        private string newTaskName;
+
+        [ObservableProperty]
+        private string newCategory;
+
+        public MainViewModel()
+        {
+            _database = new ToDoDB();
+            TodoItems = new ObservableCollection<ToDoItem>();
+            LoadItemsCommand.Execute(null);
+        }
+
+        [RelayCommand]
+        private async Task LoadItems()
+        {
+            var items = await _database.GetItems();
+            if (SelectedCategory == "Все"){
+                TodoItems = new ObservableCollection<ToDoItem>(items);
+            }
+            else
+            {
+                var filteredItems = items.Where(item => item.Category == SelectedCategory);
+                TodoItems = new ObservableCollection<ToDoItem>(filteredItems);
+            }
+        }
+
+        partial void OnSelectedCategoryChanged(string value)
+        {
+            LoadItemsCommand.Execute(null);
+        }
+
+        [RelayCommand]
+        private async Task AddItem()
+        {
+            if ((!string.IsNullOrWhiteSpace(NewTaskName)) && (!string.IsNullOrWhiteSpace(NewCategory)))
+            {
+                var newItem = new ToDoItem { ItemName = NewTaskName, Category = NewCategory, IsCompleted = false };
+                await _database.SaveItem(newItem);
+
+                NewTaskName = string.Empty;
+                NewCategory = string.Empty;
+                await LoadItemsCommand.ExecuteAsync(null);
+            }
+        }
+
+        [RelayCommand]
+        private async Task DeleteItem(ToDoItem item)
+        {
+            if (item != null)
+            {
+                await _database.DeleteItem(item);
+                await LoadItemsCommand.ExecuteAsync(null);
+            }
+        }
+    }
+}
