479cbbf6fe84ba322f56909cc28e6aa7d0bae408
KoIvRo <korot4ev29@gmail.com>
2025-09-30 18:43:27 +1000

    Уведомления

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -40,6 +40,9 @@ namespace ToDoList.ViewModels
         private ObservableCollection<ToDoItem> doneItems;
 
         [ObservableProperty]
+        private ObservableCollection<ToDoItem> overDueItems;
+
+        [ObservableProperty]
         private string newTaskName;
 
         [ObservableProperty]
@@ -49,14 +52,17 @@ namespace ToDoList.ViewModels
         private TimeSpan newEndTime;
 
         [ObservableProperty]
-        private DateTime? newEndDate;
+        private DateTime? newEndDate = DateTime.Today;
 
         public MainViewModel()
         {
             _database = new ToDoDB();
             TodoItems = new ObservableCollection<ToDoItem>();
             DoneItems = new ObservableCollection<ToDoItem>();
+            OverDueItems = new ObservableCollection<ToDoItem>();
             LoadItemsCommand.Execute(null);
+
+            _ = OverDueChecker();
         }
 
         [RelayCommand]
@@ -64,14 +70,16 @@ namespace ToDoList.ViewModels
         {
             var items = await _database.GetItems();
             if (SelectedCategory == "Все"){
-                TodoItems = new ObservableCollection<ToDoItem>(items.Where(item => item.IsCompleted == false));
+                TodoItems = new ObservableCollection<ToDoItem>(items.Where(item => !item.IsCompleted && !item.IsOverDue));
                 DoneItems = new ObservableCollection<ToDoItem>(items.Where(item => item.IsCompleted == true));
+                OverDueItems = new ObservableCollection<ToDoItem>(items.Where(item => item.IsOverDue == true));
             }
             else
             {
                 var filteredItems = items.Where(item => item.Category == SelectedCategory);
-                TodoItems = new ObservableCollection<ToDoItem>(filteredItems.Where(item => item.IsCompleted == false));
+                TodoItems = new ObservableCollection<ToDoItem>(filteredItems.Where(item => !item.IsCompleted && !item.IsOverDue));
                 DoneItems = new ObservableCollection<ToDoItem>(filteredItems.Where(item => item.IsCompleted == true));
+                OverDueItems = new ObservableCollection<ToDoItem>(filteredItems.Where(item => item.IsOverDue == true));
             }
         }
 
@@ -97,16 +105,18 @@ namespace ToDoList.ViewModels
         [RelayCommand]
         private async Task AddItem()
         {
-            if ((!string.IsNullOrWhiteSpace(NewTaskName)) && (!string.IsNullOrWhiteSpace(NewCategory)))
+            if (!string.IsNullOrWhiteSpace(NewTaskName) && 
+                !string.IsNullOrWhiteSpace(NewCategory))
             {
                 var newItem = new ToDoItem { ItemName = NewTaskName, Category = NewCategory, EndTime = NewEndTime, EndDate = NewEndDate, IsCompleted = false };
                 await _database.SaveItem(newItem);
 
                 NewTaskName = string.Empty;
-                NewCategory = string.Empty;
+                NewCategory = "Личное";
                 await LoadItemsCommand.ExecuteAsync(null);
             }
         }
+
 
         [RelayCommand]
         private async Task DeleteItem(ToDoItem item)
@@ -115,6 +125,62 @@ namespace ToDoList.ViewModels
             {
                 await _database.DeleteItem(item);
                 await LoadItemsCommand.ExecuteAsync(null);
+            }
+        }
+
+        private async Task OverDueComplete(List<ToDoItem> items)
+        {
+            foreach (ToDoItem item in items)
+            {
+                item.IsOverDue = true;
+
+                await _database.SaveItem(item);
+            }
+            LoadItemsCommand.Execute(null);
+        }
+
+        private async Task CheckOverDue()
+        {
+            var CheckItems = new List<ToDoItem>();
+            foreach (ToDoItem item in TodoItems)
+            {
+                if (item.EndDate < DateTime.Today ||
+                   (item.EndDate == DateTime.Today &&
+                   (item.EndTime < DateTime.Now.TimeOfDay)))
+                {
+                    CheckItems.Add(item);
+                }
+            }
+            if (CheckItems.Count > 0)
+            {
+                ShowOverduePushNotification(CheckItems);
+                await OverDueComplete(CheckItems);
+            }
+        }
+
+        private async Task OverDueChecker()
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(15));
+                await CheckOverDue();
+            }
+        }
+
+        private async Task ShowOverduePushNotification(List<ToDoItem> items)
+        {
+            if (items.Count == 1)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    $"ЗАДАЧА {items[0].ItemName} ПРОСРОЧЕНА!",
+                    "У вас есть просроченные задачи",
+                    "ОК");
+            }
+            else {
+                await Application.Current.MainPage.DisplayAlert(
+                    "ЗАДАЧИ ПРОСРОЧЕНЫ!",
+                    $"У вас есть {items.Count} просроченных задач",
+                    "ОК");
             }
         }
     }
